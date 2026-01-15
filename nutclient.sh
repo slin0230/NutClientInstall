99655e728dacd88765834cb9becc08f08e9f6c29
stevel <slin@pss.edu.mh>
2026-01-16 10:10:06 +1200

    added nutconfig files

--- nutclient.sh
+++ nutclient.sh
@@ -6,7 +6,7 @@ apt install -y nut-client
 # Download and extract configuration files
 if [ ! -f "nutconfig.tar.gz" ]; then
   echo "Downloading Configuration..."
-  wget https://raw.githubusercontent.com/steven-geo/proxmox-nut/refs/heads/master/nutconfig.tar.gz -O nutconfig.tar.gz
+  wget https://github.com/slin0230/NutClientInstall/raw/refs/heads/main/nutconfig.tar.gz -O nutconfig.tar.gz
 else
   echo "Using Existing Configuration tarball"
 fi
@@ -15,9 +15,6 @@ tar -xvzf nutconfig.tar.gz -C /etc
 echo "Configuring NUT"
 # Edit upsmon.conf - update password to match upsd.users
 sed -i "s/^MONITOR.*/MONITOR ups@192.168.84.243 1 upsmon pssupsmonitor2024 master/g" /etc/nut/upsmon.conf
-
-# remove unused files
-rm /etc/nut/ups.conf /etc/nut/upsd.* /etc/nut/nut.conf
 
 echo "MODE=netclient" >/etc/nut/nut.conf
 
